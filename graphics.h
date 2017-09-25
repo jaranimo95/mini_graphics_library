cc9263e94651dcaa8e24d94b8c5e6d768cc1fc59
Christian Jarani <majula@majula.local>
2017-09-25 12:57:20 -0400

    First

---
+++ graphics.h
@@ -0,0 +1,30 @@
+//
+//  driver.c
+//  gLib
+//
+//  Created by Christian Jarani on 9/15/17.
+//  Copyright © 2017 majula. All rights reserved.
+//
+
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+
+typedef unsigned short color_t;
+#define RGB(r,g,b) (r << 11 | g << 5 | b) // RGB color macro, with red being RGB(31,0,0)
+
+void  init_graphics();
+void  exit_graphics();
+char  getkey();
+void  sleep_ms(long);
+void* new_offscreen_buffer();
+void  blit(void *);
+void  clear_screen(void*);
+void  draw_pixel(void*, int, int, color_t);
+void  draw_line(void*, int, int, int, int, color_t);
+
+#endif
---
+++ library.c
@@ -0,0 +1,51 @@
+//
+//  main.c
+//  gLib
+//
+//  Created by Christian Jarani on 9/13/17.
+//  Copyright © 2017 majula. All rights reserved.
+//
+
+#include "graphics.h"
+#include <stdio.h>
+
+int file_descriptor;
+void* file_address;
+
+int    vres;		// virtual resolution
+int	   depth;		// bit depth
+size_t length;		// length of memory allocation in mmap
+
+void init_graphics()
+{
+	file_descriptor = open("/dev/fb0", O_RDWR);					// Open framebuffer
+	if(file_descriptor == -1) {									// Check if frambuffer was opened properly
+		perror("/dev/fb0 not opening properly.");					// If not, say so
+	}
+	else perror("shit worked yo");									// Else we GOOD BOIIIIIII
+
+	struct fb_fix_screeninfo fix_info;							// Lets us get info about our video card's memory
+	struct fb_var_screeninfo var_info;							// Lets us get info about our video card's display properties
+	ioctl(file_descriptor, FBIOGET_FSCREENINFO, &fix_info);
+	ioctl(file_descriptor, FBIOGET_VSCREENINFO, &var_info);	
+	 depth = fix_info.line_length;
+	  vres = var_info.yres_virtual; 
+ 	length = depth * vres;
+
+ 	file_address = mmap(0,length,PROT_WRITE,MAP_SHARED,file_descriptor,0); // Map file to memory and get that #hot #wet #address #space #babYYYYYYYY
+}
+
+void exit_graphics() {}
+char getkey() {}
+void sleep_ms(long ms) {}
+void* new_offscreen_buffer() {}
+
+void blit(void *src) {}
+void clear_screen(void *img) {}
+void draw_pixel(void *img, int x, int y, color_t color) {}
+void draw_line(void *img, int x1, int y1, int width, int height, color_t c) {}
+
+int main() {
+	init_graphics();
+	return 0;
+}
